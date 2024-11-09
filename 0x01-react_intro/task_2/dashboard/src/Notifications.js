d90d0a8a52bde2fa133112c99e9f2f83a6711a2c
kess4obehi <kessiena4excellence@gmail.com>
2024-11-09 02:00:03 +0100

    files and directories

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,32 @@
 import React from 'react';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
+import close from './close-icon.png';
 
 export default function Notifications() {
+
+    const styles = {
+        position: 'relative',
+        left: '1175px',
+        bottom: 'none',
+        background: 'transparent'
+    };
+
+    const handleClose = () => {
+        console.log('Close button has been clicked');
+      };
+
     return (
         <div className="Notifications">
             <p>Here is the list of notifications</p>
+            <button style={styles} aria-label="Close" onclick={handleClose}>
+                <img src={close} alt="Close" width="10px"/>
+            </button>
+            <ul>
+            <li data-priority="default">New course available</li>
+            <li data-priority="urgent">New resume available</li>
+                <li data-priority="urgent" dangerouslySetInnerHTML={{__html: notification}}></li>
+            </ul>
         </div>
     );
 }
--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -9,4 +9,8 @@ export function getFullYear() {
     } else {
       return "Holberton School main dashboard";
     }
-  }  
+  }
+
+  export function getLatestNotification() {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+  }
